f3e00de7c37e8ba8e688ee22aedd92c12504bfbf
marinadcrossi <marinadcrossi@gmail.com>
2025-08-05 15:00:35 -0300

    ff

--- data_loaders.py
+++ data_loaders.py
@@ -5,25 +5,25 @@ import pandas as pd
 import streamlit as st
 
 
-@st.cache_data
+#@st.cache_data
 def load_bolsas(path):
     df = pd.read_excel(path, sheet_name="bolsas", engine="openpyxl")
     df["Dates"] = pd.to_datetime(df["Dates"])
     return df
 
-@st.cache_data
+#@st.cache_data
 def load_cambio(path):
     df = pd.read_excel(path, sheet_name='moedas', engine="openpyxl")
     df["Dates"] = pd.to_datetime(df["Dates"])
     return df
 
-@st.cache_data
+#@st.cache_data
 def load_commodities_usd(path):
     df = pd.read_excel(path, sheet_name='commodities_usd', engine="openpyxl")
     df["Dates"] = pd.to_datetime(df["Dates"])
     return df
 
-@st.cache_data
+#@st.cache_data
 def load_commodities_brl(path):
     df = pd.read_excel(path, sheet_name='commodities_brl', engine="openpyxl")
     df["Dates"] = pd.to_datetime(df["Dates"])
